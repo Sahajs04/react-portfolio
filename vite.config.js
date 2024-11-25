01f5ae4a0a9bd4ca7290d8263ba1c296fb40756e
Sahajs04 <Sahaj@vt.edu>
2024-11-25 00:09:45 -0500

    testing

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: "camelCase"
-    }
-
-  }
-})
+    base: '/react-portfolio/', // Replace <repository-name> with your GitHub repository name
+});
