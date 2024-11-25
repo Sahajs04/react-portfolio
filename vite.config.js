e44bfe6e5c6aadb808dccdbaf75c680ff3da35c5
Sahajs04 <Sahaj@vt.edu>
2024-11-25 00:18:36 -0500

    testing

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/react-portfolio/', // Use your GitHub repository name here
   plugins: [react()],
   esbuild: {
       jsxInject: `import React from 'react'`,
   },
 });
+
