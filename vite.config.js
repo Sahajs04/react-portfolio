4b7f0409a111e94b9b4c424312ea983a97ecc432
Sahajs04 <Sahaj@vt.edu>
2024-11-25 00:13:44 -0500

    wefwewef

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/react-portfolio/', // Replace <repository-name> with your GitHub repository name
+    plugins: [react()],
 });
